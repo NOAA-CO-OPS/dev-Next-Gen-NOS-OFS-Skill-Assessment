Fix day slice in SCHISM station netcdf time.units string
The time.units format string used start_date_full[4:6] (month) for both
the month and day fields. Corrected the day field to use [6:8].

bin/utils/process_schism_stations_cli.py
@@ -535,7 +535,7 @@ def process_schism_stations(prop, logger):
                         time = ncfile.createVariable('time', np.float32, ('time'))
                         time.units = (f'seconds since {prop.start_date_full[0:4]}-'
                                       f'{prop.start_date_full[4:6]}-'
-                                      f'{prop.start_date_full[4:6]} '
+                                      f'{prop.start_date_full[6:8]} '
                                       f'{prop.start_date_full[-2:]}:00:00')
                         # Do rest of vars
                         lon = ncfile.createVariable('lon', np.float32, ('station'))